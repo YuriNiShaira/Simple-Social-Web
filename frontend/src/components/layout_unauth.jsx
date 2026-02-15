d03b19538a6068286b030ba9613f9c00f3448594
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-02-15 23:36:15 +0800

    fix the bg color

--- frontend/src/components/layout_unauth.jsx
+++ frontend/src/components/layout_unauth.jsx
@@ -3,7 +3,7 @@ import Navbar_Unauth from "./navbar_unauth"
 
 const Layout_Unauth = ({children}) => {
     return(
-        <VStack w='100vw' minH='100vh' bg='#FCFCFC'>
+        <VStack w='100vw' minH='100vh' bgGradient='linear(red.100 0%, yellow.100 25%, pink.100 50%)'>
             <Navbar_Unauth/>
             <Box w='100%'>
                 {children}
